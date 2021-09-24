45d01ccf0cd1d96eff3c6c279f900d6bccc1ab88
tfs-tada <tfs.tada@gmail.com>
2021-09-24 20:00:25 +0900

    enhancement: チェックボックス処理

--- src/components/organisms/ResasViewerBox/index.tsx
+++ src/components/organisms/ResasViewerBox/index.tsx
@@ -1,15 +1,45 @@
 import { FC, useEffect, useState } from 'react'
 import { getRequest } from '../../../api'
 import styles from './index.module.scss'
+import {
+  prefecturesApiType,
+  prefecturesSingleDataType,
+} from '../../../interfaces/apiEndpointType'
+import CheckArea from '../../molecules/CheckArea'
+import usePopulationCompositionList from '../../../api/ApiHooks/PopulationCompositionList'
+
 const ResasViewerBox: FC = () => {
-  const [resasData, setResasData] = useState()
+  const [resasData, setResasData] = useState<prefecturesSingleDataType[]>([])
+  const [prefCodeList, setPrefCodeList] = useState<number[]>([])
+  const { data, loading } = usePopulationCompositionList(prefCodeList)
+  console.log(data)
   useEffect(() => {
-    getRequest<any>({
-      endpointUrl: '/api/v1/result/data/data/value',
-      callback: (data) => setResasData(data),
+    getRequest<prefecturesApiType>({
+      endpointUrl: '/api/v1/prefectures',
+      callback: (data) => setResasData(data.result),
     })
   }, [])
-  console.log(resasData)
-  return <>resas box</>
+  const fixPrefCodeList = (prefCode: number) => {
+    const newList = prefCodeList.includes(prefCode)
+      ? [...prefCodeList.filter((num) => num !== prefCode)]
+      : [...prefCodeList, prefCode]
+    setPrefCodeList(newList)
+  }
+  return (
+    <div className={styles.contents_wrapper}>
+      <div className={styles.checkarea_wrapper}>
+        {resasData.map((e) => (
+          <div key={e.prefCode}>
+            <CheckArea
+              isChecked={prefCodeList.includes(e.prefCode)}
+              labelText={e.prefName}
+              onChange={() => fixPrefCodeList(e.prefCode)}
+            />
+          </div>
+        ))}
+      </div>
+      <div>ここにグラフ</div>
+    </div>
+  )
 }
 export default ResasViewerBox
