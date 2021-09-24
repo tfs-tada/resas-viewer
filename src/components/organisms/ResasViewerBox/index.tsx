046bdf110c3ba38b7591aceaed8bc3f3d87d6ade
tfs-tada <tfs.tada@gmail.com>
2021-09-24 17:24:58 +0900

    enhancement: axios接続チェック

--- src/components/organisms/ResasViewerBox/index.tsx
+++ src/components/organisms/ResasViewerBox/index.tsx
@@ -1,6 +1,15 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { getRequest } from '../../../api'
 import styles from './index.module.scss'
 const ResasViewerBox: FC = () => {
+  const [resasData, setResasData] = useState()
+  useEffect(() => {
+    getRequest<any>({
+      endpointUrl: '/api/v1/result/data/data/value',
+      callback: (data) => setResasData(data),
+    })
+  }, [])
+  console.log(resasData)
   return <>resas box</>
 }
 export default ResasViewerBox
